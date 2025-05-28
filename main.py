e9e39c53b4ed21eb9970535307094d90b8efc88f
Benjamin Cabrera <be.cabrerad@duocuc.cl>
2025-05-28 14:29:42 -0400

    Cree una variable op para el menu como opciones y luego un if

--- main.py
+++ main.py
@@ -5,4 +5,10 @@ while True:
     print("1. Agregar notas")
     print("2. Calcular Promedio")
 
-    
+    op=int(input("Ingresa un digito (1,2): "))
+
+    if op == 1:
+        nota=int(input("Ingresa la cantidad de notas"))
+
+
+
